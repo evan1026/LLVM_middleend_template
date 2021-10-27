f300a20bbb782362bbd0275e50f1abdb5d528f75
Evan Allan <evanallan1026@gmail.com>
2021-10-27 14:43:27 -0500

    Make constant propagation a bit less shitty

--- src/CatConstantPropagationProcessor.hpp
+++ src/CatConstantPropagationProcessor.hpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <llvm/IR/Instructions.h>
 #include "CatDataDependencies.hpp"
+#include "CatFunction.hpp"
 
 class CatConstantPropagationProcessor {
 
     std::map<llvm::CallInst*, llvm::Value*> replacements;
+
+    llvm::Value* getReplaceValue(llvm::Value* catVar, llvm::CallInst* callValue, bool& nonConstFound);
+    void processFunction(llvm::CallInst* callInst, const std::map<llvm::Instruction*, CatDataDependencies>& dataDepsMap);
 
     public:
         void calculate(std::vector<llvm::Instruction*>& instructions, const std::map<llvm::Instruction*, CatDataDependencies>& dataDepsMap);
